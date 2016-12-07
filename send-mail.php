c970512659907dbba7c89c4bc96be0552ad7ef07
omarsoliman <omar.solimann@gmail.com>
2016-12-07 15:53:37 +0200

    NEW: Mail PHP File

---
+++ send-mail.php
@@ -0,0 +1,10 @@
+<?php
+$to      = 'omar.solimann@gmail.com.com';
+$subject = 'Github Contact';
+$message = 'Test';
+$headers = 'From: webmaster@example.com' . "\r\n" .
+    'Reply-To: webmaster@example.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+mail($to, $subject, $message, $headers);
+?>
