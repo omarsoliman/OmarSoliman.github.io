3d6e6ee2c5e520b1d00df6427497e5a0a36daec9
omarsoliman <omar.solimann@gmail.com>
2018-01-02 21:09:10 +0200

    Fix: Trial Mail

---
+++ mail.php
@@ -0,0 +1,30 @@
+<?php
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+
+    require 'PHPMailer/src/Exception.php';
+    require 'PHPMailer/src/PHPMailer.php';
+    require 'PHPMailer/src/SMTP.php';
+
+    $mail = new PHPMailer();
+    $mail->isSMTP();
+    $mail->SMTPAuth() = true;
+    $mail->SMTPSecure = 'ssl';
+    $mail->Host = 'smtp.gmail.com';
+    $mail->Post = '465';
+    $mail->isHTML();
+    $mail->Username = 'omar.solimann@gmail.com';
+    $mail->Password = '011$0lim@n';
+    $mail->SetFrom = 'info@soliman.com';
+    $mail->Subject = 'Soliman Contact';
+    $mail->Message = 'Test Message';
+    $mail->AddAddress = 'omar.solimann@gmail.com';
+    
+    $mail->Send();
+
+
+
+?>
+
+	<!--====== Javascripts & Jquery ======-->	
+	<script src="js/jquery-2.1.4.min.js"></script>
