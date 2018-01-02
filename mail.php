216215b0385efb925a35c771249149216a1dcf7b
omarsoliman <omar.solimann@gmail.com>
2018-01-02 13:22:53 +0200

    New Website

---
+++ mail.php
@@ -0,0 +1,11 @@
+<?php
+var_dump($post,$GET);exit;
+$to      = 'omar.solimann@gmail.com';
+$subject = 'Github Contact';
+$message = 'Test';
+$headers = 'From: webmaster@example.com' . "\r\n" .
+    'Reply-To: webmaster@example.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+mail($to, $subject, $message, $headers);
+?>
